8e1b0633535e3db15835d5644b8138f90cddfe67
jelon12111 <jelon1211@gmail.com>
2022-12-18 17:28:47 +0100

    user schema fixed

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
         type: String,
         required: true,
     },
@@ -9,7 +13,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    email: {
+    username: {
         type: String,
         required: true,
     },
