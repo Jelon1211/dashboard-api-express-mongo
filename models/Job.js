d21d50e6d47f7ef413e2989abb408ae4c99c161e
jelon12111 <jelon1211@gmail.com>
2022-12-18 17:32:06 +0100

    job schema

--- models/Job.js
+++ models/Job.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose')
 
 const jobSchema = new mongoose.Schema({
-    name: {
+    title: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    shortdescription: {
+        type: String,
+        required: true,
+    },
+    longdescription: {
+        type: String,
+        required: true,
+    },
+    logo: {
         type: String,
         required: true,
     }
