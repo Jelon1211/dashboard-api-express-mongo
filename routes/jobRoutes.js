6c3db569d443dc8ca2b74a02bddc3ee3cb7aff03
jelon12111 <jelon1211@gmail.com>
2022-12-27 10:47:03 +0100

    disabled jwt

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -4,7 +4,7 @@ const jobsController = require('../controllers/jobsController')
 
 const verifyJWT = require('../middleware/verifyJWT')
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
 router.route('/')
     .get(jobsController.getAllJobs)
