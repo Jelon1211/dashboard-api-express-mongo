d5ae03a647aea354608b61b664c597f1a189aedc
jelon12111 <jelon1211@gmail.com>
2023-01-09 16:15:11 +0100

    z

--- models/Candidate.js
+++ models/Candidate.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
 const candidateSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        date: {
-            type: Date,
-            required: true,
-        },
-        shortdescription: {
-            type: String,
-            required: true,
-        },
-        longdescription: {
-            type: String,
-            required: true,
-        },
-        logo: {
-            type: String,
-            required: true,
-        }
-}, 
-{
-    timestams: true
-}
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    shortdescription: {
+      type: String,
+      required: true,
+    },
+    longdescription: {
+      type: String,
+      required: true,
+    },
+    logo: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestams: true,
+  }
 );
 
 // candidateSchema.plugin(AutoIncrement, {
@@ -35,4 +35,4 @@ const candidateSchema = new mongoose.Schema(
 //     start_seq: 500
 // })
 
-module.exports = mongoose.model('Candidate', candidateSchema)
+module.exports = mongoose.model("Candidate", candidateSchema);
